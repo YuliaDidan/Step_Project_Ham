c041f227aa11eeeb285983a51397470fca8d10c2
Yulia Didan <ydidan2809@gmail.com>
2018-11-02 12:45:07 +0200

    services finished

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 
-// import $ from "jquery";
+import $ from "jquery";
 
 // import "jquery-ui";
 
@@ -10,7 +10,50 @@
 // $( "#tabs2" ).tabs();
 
 
+let selectedItem = $(`.services__item`);
 
+selectedItem.click(function(evt){   
+    evt.preventDefault();          
+    if (selectedItem.hasClass("services__item--selected")){
+        
+        selectedItem.removeClass("services__item--selected");
+    }
+    $(this).toggleClass(`services__item--selected`);
+    
+
+});
+
+$(".services__item:eq(0)").click();
+
+let selectedLink = $(`.services__link`);
+
+selectedLink.on('click', function(evt){
+    evt.preventDefault();
+    $(`.services__wrap`).hide();
+              
+    if (selectedLink.hasClass("services__link--selected")){
+        
+        selectedLink.removeClass("services__link--selected");
+    }
+    $(this).toggleClass(`services__link--selected`);
+    let tabid = $(this).attr(`href`);
+    console.log(tabid);
+    $(tabid).show();
+    
+});
+
+$(".services__link:eq(0)").click();
+
+
+$("button").on('click', function () {
+    $(".content").hide();
+    $("button.selected").removeClass('selected');
+    $(this).toggleClass('selected')
+    let tabid = $(this).attr('data-tab');
+    $("#" + tabid).show();
+})
+
+$("button:eq(1)").click();
 
 
 
