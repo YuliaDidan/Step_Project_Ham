e2382c84184b48795bc26460cd252e4787da7478
Yulia Didan <ydidan2809@gmail.com>
2018-11-03 21:11:56 +0200

    works-gallery in progress

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
 
-import $ from "jquery";
+// import $ from "jquery";
 
 // import "jquery-ui";
 
-// $( "#tabs" ).tabs();
+document.addEventListener('DOMContentLoaded', init);
 
+function init() {
+  const d = document;
+  const page = d.getElementById('homepage') || d.getElementById('about');
+  const id = page.getAttribute('id');
 
-
-// $( "#tabs2" ).tabs();
-
+  switch (id) {
+    case 'homepage': {
+      console.log('Hello, from Homepage');
+      break;
+    }
+    case 'about': {
+      console.log('Hello, from About page');
+      break;
+    }
+    default: {
+      // ...
+    }
+  }
+}
 
 let selectedItem = $(`.services__item`);
 
@@ -18,8 +33,7 @@ selectedItem.click(function(evt){
         
         selectedItem.removeClass("services__item--selected");
     }
-    $(this).toggleClass(`services__item--selected`);
-    
+    $(this).toggleClass(`services__item--selected`);   
 
 });
 
@@ -36,47 +50,133 @@ selectedLink.on('click', function(evt){
         selectedLink.removeClass("services__link--selected");
     }
     $(this).toggleClass(`services__link--selected`);
-    let tabid = $(this).attr(`href`);
-    console.log(tabid);
+    let tabid = $(this).attr(`href`);    
     $(tabid).show();
     
 });
 
 $(".services__link:eq(0)").click();
 
+let selectedWorksLink = $(`.works__menu-item-link`);
 
-$("button").on('click', function () {
-    $(".content").hide();
-    $("button.selected").removeClass('selected');
-    $(this).toggleClass('selected')
-    let tabid = $(this).attr('data-tab');
-    $("#" + tabid).show();
-})
+selectedWorksLink.on('click', function(evt){
+    evt.preventDefault();
+    $(`.works__gallery`).hide();
+              
+    if (selectedWorksLink.hasClass("works__menu-item-link--selected")){
+        
+        selectedWorksLink.removeClass("works__menu-item-link--selected");
+    }
+    $(this).toggleClass(`works__menu-item-link--selected`);
+    let tabid = $(this).attr(`href`);    
+    $(tabid).show();
+    
+});
 
-$("button:eq(1)").click();
+$(".works__menu-item-link:eq(0)").click();
 
+let worksBtn = $(`.works__btn`);
 
+ function goLoader () {    
+    let loader = $(`.holder`)
+    // console.log(loader.length);
+    loader.show(4000, function() {        
+        // loader.css(`display`, `none`);
+        loader.hide();
+    });
+}
 
-
-
-// document.addEventListener('DOMContentLoaded', init);
-
-// function init() {
-//   const d = document;
-//   const page = d.getElementById('homepage') || d.getElementById('about');
-//   const id = page.getAttribute('id');
-
-//   switch (id) {
-//     case 'homepage': {
-//       console.log('Hello, from Homepage');
-//       break;
+// function addImages(itemSize=12) {
+//     let arrayElements = [];
+//     for (let i=0; i<itemSize; i++){
+//         // let elem = $(`<img src="./img/works/graphic_design/gd${i+1}.jpg" alt="picture" class="works__img">`);
+//         let elem = $(`<img>`);
+//         elem.attr('src', `./img/works/graphic_design/gd${i+1}.jpg`);
+//         elem.attr(`alt`, `images${i +1}`);
+//         arrayElements.push(elem);        
 //     }
-//     case 'about': {
-//       console.log('Hello, from About page');
-//       break;
-//     }
-//     default: {
-//       // ...
-//     }
-//   }
+//     $(`.works__img-container`).appendChild(arrayElements);
 // }
+
+let itemSize=12;
+let maxGalleryLength = 36;
+let count = 0;
+
+// function addImages() {   
+//     let arrayElements = [];
+//     for (let i=0; i<itemSize; i++){
+//         // let elem = $(`<img src="./img/works/graphic_design/gd${count%itemSize}.jpg" alt="picture" class="works__img">`);
+//         let elem = $(`<img>`);
+//         elem.attr('src', `https://picsum.photos/200/300/?image=${count*3}`)
+//         .attr(`alt`, `images${count + 1}`)
+//         .addClass(`works__img`)
+//         .wrap(`<div class="col-3"></div>`)
+//         .parent()
+//         // .addClass(`col-3`)
+//         .on(`click`, imageClick);
+//         arrayElements.push(elem); 
+//         count++;       
+//     }  
+//     if(count === maxGalleryLength){
+//         worksBtn.hide();
+//     }
+//     $(`.works__img-container`).append(arrayElements);
+// }
+
+function addImages() {   
+    let arrayElements = [];
+    for (let i=0; i<itemSize; i++){
+        let elem = $(`<div class="col-3"><img src="https://picsum.photos/200/200/?image=${count*3}" alt="picture" class="works__img"></div>`);
+        // let elem = $(`<img>`);
+        // elem.attr('src', `https://picsum.photos/200/300/?image=${count*3}`)
+        // .attr(`alt`, `images${count + 1}`)
+        // .addClass(`works__img`)
+        // .wrap(`<div class="col-3"></div>`)
+        // .parent()
+        // // .addClass(`col-3`)
+        elem.on(`click`, imageClick);
+        arrayElements.push(elem); 
+        count++;       
+    }  
+    if(count === maxGalleryLength){
+        worksBtn.hide();
+    }
+    $(`.works__img-container`).append(arrayElements);
+}
+
+function imageClick() {    
+    $(this).attr(`alt`);
+    // console.log($(this).attr(`alt`));
+    // $(`previewImage`).fadeOut(1000);   
+    let clonedImage =  $(this).clone(); 
+    console.log(clonedImage.length);   
+    clonedImage.addClass(`previewImage`)
+    .css(`position`, `absolute`)
+    .css(`top`, `30%`)
+    .css(`left`, `40%`)
+    // .css(`margin`, `auto`)
+    .css(`width`, `100px`);
+    // .css(`height`, `100px`);
+    clonedImage.hide();
+    clonedImage.on(`click`, hideImage);
+    $(`.works__img-container`).append(clonedImage);
+    clonedImage.fadeIn(3000);
+}
+
+function hideImage() {
+    $(this).fadeOut(1000);
+}
+
+addImages();
+
+
+// let worksBtn = $(`.works__btn`);
+
+// console.log( worksBtn.length);
+
+worksBtn.on(`click`, addImages);
+
+
+
+
+
