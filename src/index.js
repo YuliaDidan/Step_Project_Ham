f798e8f8745c273d5369fd91c5b69c173f55729a
Yulia Didan <ydidan2809@gmail.com>
2018-11-07 16:43:04 +0200

    carusel done

--- src/index.js
+++ src/index.js
@@ -77,74 +77,130 @@ $(document).ready(function() {
     });
     
     $(".works__menu-item-link:eq(0)").click();
-    
-    let worksBtn = $(`.works__btn`);
-    
-    let itemSize=12;
-    let maxGalleryLength = 36;
-    let count = 0;
-    
-    function addImages() {    
-        let arrayElements = [];
+
+    $(function(){
+
+        let worksBtn = $(`.works__btn`);                 
+        let itemSize=12;
+        let maxGalleryLength = 36;
+        let count = 0;
+        
+        function addImagesFromResource() {       
+            let arrayElements = [];
+            for (let i=0; i<itemSize; i++){
+                let elem = $(`<div class="col-3"><img src="https://picsum.photos/200/?image=${count*2}" alt="picture" class="works__img"></div>`);
+                // let imageNum = Math.floor((Math.random() * 11) + 1);
+                // let elem = $(`<div class="col-3"><img src="./img/works/graphic_design/gd${count}.jpg" alt="picture" class="works__img"></div>`);
+                // let elem = $(`<img>`);
+                // elem.attr('src', `https://picsum.photos/200/300/?image=${count*3}`)
+                // .attr(`alt`, `images${count + 1}`)
+                // .addClass(`works__img`)
+                // .wrap(`<div class="col-3"></div>`)
+                // .parent()
+                // // .addClass(`col-3`)
+                elem.on(`click`, imageClick);
+                arrayElements.push(elem); 
+                count++;                      
+            }  
+            if(count === maxGalleryLength){
+                worksBtn.hide();
+                console.log(maxGalleryLength.length);
+            }
+
+            $(`.works__gallery--all`).append(arrayElements);
+            }
+
+            function imageClick() { 
+                $(`previewImage`).fadeOut(1000);   
+                let clonedImage =  $(this).clone();       
+                clonedImage.addClass(`previewImage`)
+                .css({
+                    'position': 'absolute',   
+                    'top': '33.3%',
+                    // 'bottom': '33%',
+                    'left': '0',
+                    'right': '0',
+                    'margin': 'auto',
+                    'width': '150%'
+                }); 
+                clonedImage.hide();   
+                clonedImage.on(`click`, hideImage);
+                $(`.works__gallery--all`).append(clonedImage);
+                clonedImage.fadeIn(3000);
+            }
+            function hideImage() {
+                $(this).fadeOut(1000);
+            }
+            
+            function goLoader() {    
+                let loader = $(`.holder`)     
+                loader.fadeIn(5000, function() {   
+                    loader.fadeOut();
+                });    
+            }
+            
+            function addImagesDelay() {
+                goLoader();
+                window.setTimeout(addImagesFromResource, 5000);
+            }
+            
+            addImagesFromResource();    
+            
+            worksBtn.on(`click`, addImagesDelay);
+    });
+
+    $(function(){
+
+    function addImagesfromFolder(itemSize, container, folderName, fileName) {        
+        // let maxGalleryLength = 12;
+        let count = 0;    
+        let arrayElements = [];             
         for (let i=0; i<itemSize; i++){
-            let elem = $(`<div class="col-3"><img src="https://picsum.photos/200/?image=${count*2}" alt="picture" class="works__img"></div>`);
-            // let imageNum = Math.floor((Math.random() * 12) + 1);
-            // let elem = $(`<img src="./img/works/graphic_design/gd${imageNum}.jpg" alt="picture" class="works__img">`);
-            // let elem = $(`<img>`);
-            // elem.attr('src', `https://picsum.photos/200/300/?image=${count*3}`)
-            // .attr(`alt`, `images${count + 1}`)
-            // .addClass(`works__img`)
-            // .wrap(`<div class="col-3"></div>`)
-            // .parent()
-            // // .addClass(`col-3`)
-            elem.on(`click`, imageClick);
+            let elem = $(`<div class="col-3"><img src="./img/works/${folderName}/${fileName}${count+1}.jpg" alt="picture" class="works__img"></div>`);
+            // elem.on(`click`, imageClick);
             arrayElements.push(elem); 
             count++;       
         }  
-        if(count === maxGalleryLength){
-            worksBtn.hide();
-        }
-        $(`.works__gallery--all`).append(arrayElements);
-        }
-    
-    
-    function imageClick() { 
-        $(`previewImage`).fadeOut(1000);   
-        let clonedImage =  $(this).clone();       
-        clonedImage.addClass(`previewImage`)
-        .css({
-            'position': 'absolute',   
-            'top': '33.3%',
-            // 'bottom': '33%',
-            'left': '0',
-            'right': '0',
-            'margin': 'auto',
-            'width': '150%'
-        }); 
-        clonedImage.hide();   
-        clonedImage.on(`click`, hideImage);
-        $(`.works__gallery--all`).append(clonedImage);
-        clonedImage.fadeIn(3000);
+        // if(count === maxGalleryLength){
+        //     worksBtn.hide();
+        // }
+        $(container).append(arrayElements);       
+        
     }
+
+    addImagesfromFolder( 12, `.works__gallery--graphic`, `graphic_design`, `gd`); 
+    addImagesfromFolder( 8, `.works__gallery--web`, `web_design`, `wd`); 
+    addImagesfromFolder( 12, `.works__gallery--landing`, `landing_pages`, `lp`); 
+    addImagesfromFolder( 10, `.works__gallery--wordpress`, `wordpress`, `wordpress`);  
     
-    function hideImage() {
-        $(this).fadeOut(1000);
-    }
-    
-    function goLoader() {    
-        let loader = $(`.holder`)     
-        loader.fadeIn(5000, function() {   
-            loader.fadeOut();
-        });    
-    }
-    
-    function addImagesDelay() {
-        goLoader();
-        window.setTimeout(addImages, 5000);
-    }
-    
-    addImages();
-    
-    worksBtn.on(`click`, addImagesDelay);
 })
 
+
+let testimonials = $('.testimonials__slider-list');
+    if (testimonials.length) {
+        // console.log(testimonials.length)
+        testimonials.slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.testimonials__slider-nav-list'
+      });
+  
+  
+      $('.testimonials__slider-nav-list').slick({
+        centerMode: true,
+        centerPadding: `80px`,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        asNavFor: '.testimonials__slider-list',        
+        swipeToSlide: true,
+        focusOnSelect: true,
+        prevArrow: $('.testimonials__slider-btn--prev'),
+        nextArrow: $('.testimonials__slider-btn--next')        
+      });
+    }
+
+
+
+})
